2a664866be42096e7372c55c1702a04c7afd3373
Terry <terry86062@gmail.com>
2019-03-29 09:18:44 +0800

    Initial Commit

---
+++ ASiC Midterm-Exam Terry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASiC Midterm-Exam Terry
+//
+//  Created by 黃偉勛 Terry on 2019/3/29.
+//  Copyright © 2019 Terry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ASiC Midterm-Exam TerryTests/ASiC_Midterm_Exam_TerryTests.swift
@@ -0,0 +1,34 @@
+//
+//  ASiC_Midterm_Exam_TerryTests.swift
+//  ASiC Midterm-Exam TerryTests
+//
+//  Created by 黃偉勛 Terry on 2019/3/29.
+//  Copyright © 2019 Terry. All rights reserved.
+//
+
+import XCTest
+@testable import ASiC_Midterm_Exam_Terry
+
+class ASiC_Midterm_Exam_TerryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
